092dda00ff760c2adc341038e78eb44c9edd1ec5
VI0LENTINE <W0516070@campus.nscc.ca>
2026-04-10 23:50:34 -0300

    Refactor CSS styles for improved layout and design consistency.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,24 +4,22 @@ import ChatWidget from "./ChatWidget";
 export default function Layout() {
     return (
         <div className="container-fluid">
-            <header>
-                <nav className="navbar mb-4">
-                    <div className="container-fluid">
-                        <Link to="/" className="navbar-brand">
-                            TechNook
-                        </Link>
+            <nav className="navbar px-3">
+                <Link to="/" className="navbar-brand">
+                    🍃 TechNook
+                </Link>
 
-                        <Link to="/cart">Cart</Link>
-                    </div>
-                </nav>
-            </header>
+                <Link to="/cart" className="text-decoration-none">
+                    🛒 Cart
+                </Link>
+            </nav>
 
-            <main className="container-fluid">
+            <main className="container py-4">
                 <Outlet />
             </main>
 
-            <footer className="container-fluid mt-5">
-                TechNook, © 2026
+            <footer className="text-center py-4">
+                &copy; 2026 TechNook
             </footer>
 
             <ChatWidget />
